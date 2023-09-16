4d483759fa388ad6049899f0507d42655ff0841c
WallaceAlli <alliwaliy12@gmail.com>
2023-09-16 13:33:35 -0500

    Wally, 1:36 pm

---
+++ Hackwest/src/App.tsx
@@ -0,0 +1,11 @@
+import Message from "./Messsage"
+function App(){
+  return <div><Message></Message></div>
+
+
+
+
+
+}
+
+export default App;
---
+++ Hackwest/src/Messsage.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+
+function Message(){
+    // JSX: Javascript XML
+    // Where we will use the component
+    return <h1>Hello World</h1>;
+
+}
+
+export default Message;
