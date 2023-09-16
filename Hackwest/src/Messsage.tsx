357cd79968560f9f2d51c400bf1ea75c1c666abc
WallaceAlli <alliwaliy12@gmail.com>
2023-09-16 13:37:27 -0500

    Waliy, 1:37pm

--- Hackwest/src/Messsage.tsx
+++ Hackwest/src/Messsage.tsx
@@ -3,7 +3,7 @@
 function Message(){
     // JSX: Javascript XML
     // Where we will use the component
-    return <h1>Hello World</h1>;
+    return <h1>Hello World Do Not Fear For I am here</h1>;
 
 }
 
